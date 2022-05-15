1c5d648424ff38b09accd4c41c3bcf00450fe72d
Dervla <36761419+Derv6464@users.noreply.github.com>
2022-05-15 21:15:07 +0100

    Add files via upload

---
+++ SCD30.py
@@ -0,0 +1,23 @@
+#https://docs.circuitpython.org/projects/scd30/en/latest/examples.html
+import time
+import board
+import adafruit_scd30
+
+# SCD-30 has tempremental I2C with clock stretching, datasheet recommends
+# starting at 50KHz
+i2c = busio.I2C(board.SCL, board.SDA,frequency=50000)
+scd = adafruit_scd30.SCD30(i2c)
+
+while True:
+    # since the measurement interval is long (2+ seconds) we check for new data before reading
+    # the values, to ensure current readings.
+    if scd.data_available:
+        print("Data Available!")
+        print("CO2:", scd.CO2, "PPM")
+        print("Temperature:", scd.temperature, "degrees C")
+        print("Humidity:", scd.relative_humidity, "%%rH")
+        print("")
+        print("Waiting for new data...")
+        print("")
+
+    time.sleep(0.5)
